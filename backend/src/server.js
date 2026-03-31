18b3e2dc00d53de840e0dd2193d5e73e2ee4ac2f
unknown <you@example.com>
2026-03-31 20:39:43 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -24,9 +24,9 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 }
 
 server.listen(PORT, () => {
